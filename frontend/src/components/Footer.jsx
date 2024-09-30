fe545d76aa292078d5fa7ba28b2eb4f99671f2e2
psharma85910 <psharma85910@gmail.com>
2024-09-30 15:21:40 +0530

    changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div>
 
-      <footer className="w-full shadow bg-gray-900">
+      <footer className="w-full shadow bg-gray-900 footer" >
         <div className=" w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm  sm:text-center text-gray-400">
             © 2024{" "}
